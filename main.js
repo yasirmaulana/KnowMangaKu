2bba67c87ea80836380ea82a159af8be3a7045a8
yasirmaulana <yasir.maulana@gmail.com>
2018-03-02 23:12:27 +0700

    update projek

---
+++ main.js
@@ -0,0 +1,20 @@
+// seleksi menu about
+var navigasi = document.getElementById('myNavbar')
+var about = 'Know MangaKu adalah sebuah tes pengetahuan tentang manga Dragon Ball dan One Piece.'
+
+navigasi.children[0].children[1].children[0].addEventListener('click', function(){
+  alert(about)
+})
+
+// =============================================================
+// seleksi kuis dragon ball
+var flaglis = ''
+var gambarDB = document.getElementsByClassName('row')[0]
+gambarDB.children[0].addEventListener('click',
+function(){
+  alert('hahahah')
+}
+)
+
+function kontenAturan(){
+}
